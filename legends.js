85b9b5ea916098376aceeceaf7b6ba2aefdae3df
ccheng3 <ccrcheng@ucdavis.edu>
2021-02-28 15:41:10 -0800

    added each legend's title and decreased line height btwn name and title to up UX

--- legends.js
+++ legends.js
@@ -5,16 +5,28 @@ legendNames = ["Fuse", "Bangalore",
    "Gibraltar", "Horizon", "Lifeline",
    "Loba", "Mirage", "Octane", "Pathfinder",
    "Rampart", "Revenant", "Wattson",
-   "Wraith"]
+   "Wraith"];
+legendRoles = ["Explosives Enthusiast", "Professional Soldier",
+   "Technological Tracker", "Toxic Trapper",
+   "Surveillance Expert", "Shielded Fortress", "Gravitational Manipulator",
+   "Combat Medic", "Translocating Thief", "Holographic Trickster",
+   "High-Speed Daredevil", "Forward Scout", "Base of Fire",
+   "Synthetic Nightmare", "Static Defender", "Interdimensional Skirmisher"];
 for (let i = 0; i < 16; ++i) {
    const imgAndDescription = document.createElement("div");
    let image = document.createElement("img");
    image.setAttribute("src", `Sources/Legends/Legend_${i + 1}.jpg`);
    let legendName = document.createElement("div");
    legendName.textContent = legendNames[i];
+   let legendRole = document.createElement("div");
+   legendRole.textContent = legendRoles[i];
    // append legend and their name to smaller div
    imgAndDescription.appendChild(image);
    imgAndDescription.appendChild(legendName);
+   imgAndDescription.appendChild(legendRole);
+   legendName.style.lineHeight = "85%";
+   legendRole.style.fontSize = "1.5rem";
+   legendRole.style.fontStyle = "italic";
    imgAndDescription.setAttribute("id", `legendInfo_${i + 1}`);
    imgAndDescription.style.textAlign = "center";
    imgAndDescription.style.fontSize = "2rem";
