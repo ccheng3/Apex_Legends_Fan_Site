0458d15d0e2d0caccbb5d31587594b1ac9269e77
ccheng3 <ccrcheng@ucdavis.edu>
2021-02-28 19:15:14 -0800

    completed Season 8 Meta page

---
+++ meta.js
@@ -0,0 +1,6 @@
+const pageTitle = document.createElement("div");
+pageTitle.textContent = "Season 8 Gameplay Meta & Competitive Ranked Tips";
+pageTitle.setAttribute("id", "pageTitle");
+document.body.insertBefore(pageTitle, document.getElementById("compTips"));
+
+
